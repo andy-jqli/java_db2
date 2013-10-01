89964a56f22a104cb3b32f4087af17966ab98119
Uriel Hdz. Camacho <uriel.hdzc@gmail.com>
2013-10-01 14:33:53 -0500

    first commit

---
+++ src/ibmconexion/IbmConexion.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ibmconexion;
+
+/**
+ *
+ * @author Uriel
+ */
+public class IbmConexion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws ClassNotFoundException {
+        Conexion c = new Conexion("dbuser1","123456","192.168.1.69",50000,"equipo7");
+    }
+}
