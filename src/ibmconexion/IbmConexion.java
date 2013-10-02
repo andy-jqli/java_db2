7d4c0ffd15d0b3fc98735c35374bc9cdb53003a2
Uriel Hdz. Camacho <uriel.hdzc@gmail.com>
2013-10-01 21:33:01 -0500

    Agregado mysql jdbc y tratando de ejecutar algunas sentencias

--- src/ibmconexion/IbmConexion.java
+++ src/ibmconexion/IbmConexion.java
@@ -4,6 +4,9 @@
  */
 package ibmconexion;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author Uriel
@@ -13,7 +16,18 @@ public class IbmConexion {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Conexion c = new Conexion("db2admin","123456","192.168.1.69",50000,"equipo7");
+        //Conexion_java cj = new Conexion_java("192.168.1.69", "root", "", "equipo7");
+        int i = 0;
+        ResultSet rs = c.buscar("SELECT * FROM ciudad");
+        //System.out.println(""+i);
+        /*
+        while(i < 5){
+            if(c.insertar("INSERT INTO ciudad(idCiudad,nombre) VALUES("+i+",ciudad"+i+")")){
+                System.out.println(":)");
+            }
+            i++;
+        }*/
     }
 }
