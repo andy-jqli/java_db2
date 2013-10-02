67609043902b43a541c391d9877dc5d224051a82
Uriel Hdz. Camacho <uriel.hdzc@gmail.com>
2013-10-01 22:40:30 -0500

    Se agregaron los scripts para llenar la tabla

--- src/ibmconexion/IbmConexion.java
+++ src/ibmconexion/IbmConexion.java
@@ -6,6 +6,7 @@ package ibmconexion;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 /**
  *
@@ -17,17 +18,47 @@ public class IbmConexion {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Conexion c = new Conexion("db2admin","123456","192.168.1.69",50000,"equipo7");
-        //Conexion_java cj = new Conexion_java("192.168.1.69", "root", "", "equipo7");
-        int i = 0;
-        ResultSet rs = c.buscar("SELECT * FROM BD.ciudad");
-        //System.out.println(""+i);
-        /*
-        while(i < 5){
-            if(c.insertar("INSERT INTO ciudad(idCiudad,nombre) VALUES("+i+",ciudad"+i+")")){
+        
+        //Conexion a DB2
+        Conexion c = new Conexion("db2admin","123456","127.0.0.1",50000,"equipo7");
+        //Conexion a mySQL
+        Conexion_java cj = new Conexion_java("127.0.0.1", "root", "", "equipo7");
+        
+        //Populate the DB2 TABLE
+        int i = 1;        
+        while(i <= 5){
+            if(c.insertar("INSERT INTO BD.ciudad(idCiudad,nombre) VALUES("+i+",Ciudad"+i+")")){
                 System.out.println(":)");
             }
             i++;
-        }*/
+        }
+        System.out.println("Las tabla ciudad tiene "+c.contarRegistros("BD.ciudad")+" registros");
+        
+        //Populate the mySQL TABLE
+        /*
+        int i = 1;
+        Random r = new Random();
+        while(i <= 10){
+            int random = getRandom(1,5,r);
+            if(c.insertar("INSERT INTO alumno(idAlumno,nombre,idCiudad) VALUES("+i+",Alumno"+i+",Ciudad"+random+")")){
+                System.out.println(":)");
+            }
+            i++;
+        }
+        System.out.println("Las tabla alumno tiene:  "+cj.contarRegistros("alumno")+" registros");
+        */
+    }
+    
+    private static int getRandom(int aStart, int aEnd, Random aRandom){
+        if (aStart > aEnd) {
+            throw new IllegalArgumentException("Start cannot exceed End.");
+        }
+    
+        long range = (long)aEnd - (long)aStart + 1;
+    
+        long fraction = (long)(range * aRandom.nextDouble());
+        int randomNumber =  (int)(fraction + aStart);    
+    
+        return randomNumber;
     }
 }
